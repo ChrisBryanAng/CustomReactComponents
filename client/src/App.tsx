6c8d232d2a74a38ad9dc125789af3fb693fa1248
ChrisBryanAng <ChrisBryanAng@gmail.com>
2023-02-25 14:45:40 -0800

    Initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return <div className='text-xl text-red-200'>Hello</div>;
+}
+
+export default App;
