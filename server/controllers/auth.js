706c54326aebb2bd3f1e03bb412497b8e718785b
kfulkerson-pcg <kfulkerson@phxcapitalgroup.com>
2023-03-03 17:23:36 -0800

    add: global error middleware for catching all failures throughout backend endpoints, add: controller function for signing in a user that takes in an email-password as a body and returns a token that can be used for authorization, add: eslint config for server folder

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,48 +5,99 @@ import asyncHandler from 'express-async-handler';
 import User from '../mongodb/models/user.js';
 
 export const signup = asyncHandler(async (req, res, next) => {
-	const { firstName, lastName, email, password } = req.body;
+  const {
+    firstName, lastName, email, password,
+  } = req.body;
 
-	if (!firstName || !lastName || !email || !password) {
-		res.status(400);
-		throw new Error('Please add all fields');
-	}
+  if (!firstName || !lastName || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
 
-	//check if the user already exists
-	const existingUser = await User.findOne({ email });
-	if (existingUser) {
-		res.status(400);
-		throw new Error('User with that email already exists');
-	}
+  // check if the user already exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    res.status(400);
+    throw new Error('User with that email already exists');
+  }
 
-	const user = await User.create({
-		firstName,
-		lastName,
-		email,
-		password: bcrypt.hashSync(password, 10),
-	});
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password: bcrypt.hashSync(password, 10),
+  });
 
-	const token = jwt.sign(
-		{
-			email: user.email,
-			firstName: user.firstName,
-			lastName: user.lastName,
-		},
-		process.env.JWT_SECRET,
-		{
-			expiresIn: '30d',
-		}
-	);
+  const token = jwt.sign(
+    {
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    },
+  );
 
-	if (user) {
-		res.status(201).json({
-			success: true,
-			data: {
-				email: user.email,
-				firstName: user.firstName,
-				lastName: user.lastName,
-				token,
-			},
-		});
-	}
+  if (user) {
+    res.status(201).json({
+      success: true,
+      data: {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        token,
+      },
+    });
+  }
+});
+
+export const signin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  // check if the user already exists
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser === null) {
+    res.status(400);
+    throw new Error('User does not exist');
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    res.status(400);
+    throw new Error('Invalid Credentials, Passwords do not match');
+  }
+
+  const token = jwt.sign(
+    {
+      id: existingUser._id,
+      email: existingUser.email,
+      firstName: existingUser.firstName,
+      lastName: existingUser.lastName,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    },
+  );
+
+  if (existingUser) {
+    const stringIds = existingUser.favoriteCars.map((id) => id.toString());
+    res.status(201).json({
+      success: true,
+      data: {
+        _id: existingUser._id,
+        fullName: existingUser.fullName,
+        email: existingUser.email,
+        favorites: stringIds,
+        token,
+      },
+    });
+  }
 });
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import errorHandler from './middleware/errorHandler.js';
 import connectDB from './mongodb/connect.js';
 import AuthRoutes from './routes/auth.js';
 
@@ -12,20 +13,21 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', async (req, res) => {
-	res.status(200).json({
-		message: 'Test',
-	});
+  res.status(200).json({
+    message: 'Test',
+  });
 });
 
 app.use('/auth', AuthRoutes);
+app.use(errorHandler);
 
 const startServer = async () => {
-	try {
-		connectDB(process.env.MONGODB_URL);
-		app.listen(9000, () => console.log('Server started on port 9000'));
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(9000, () => console.log('Server started on port 9000'));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
